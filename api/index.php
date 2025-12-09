57a1a875d83aa26ea584bd6be636c3a7ea2634bd
Valentino812 <untarvalen123@gmail.com>
2025-12-09 20:43:32 +0700

    debug

--- api/index.php
+++ api/index.php
@@ -1,2 +1,16 @@
 <?php
-require __DIR__ . '/../public/index.php';
+// require __DIR__ . '/../public/index.php';
+
+// Aktifkan error reporting kasar
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Throwable $e) {
+    // Tampilkan error ke layar browser
+    echo '<h1>Fatal Error</h1>';
+    echo '<pre>' . $e->getMessage() . '</pre>';
+    echo '<pre>' . $e->getTraceAsString() . '</pre>';
+}
